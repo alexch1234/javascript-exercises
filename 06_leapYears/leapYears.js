db14aefffa4a0bca2a3f69a30cba45b408624854
Alex Chang <75132115+alexch1234@users.noreply.github.com>
2023-12-07 20:26:30 +0000

    feat: Complete exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    divisibleBy4 = year % 4 === 0;
+    divisibleBy100 = year % 100 === 0;
+    divisibleBy400 = year % 400 === 0;
 
+    if (divisibleBy4) {
+        if (divisibleBy100) {
+            return divisibleBy400 ? true : false;
+        }
+        return true;  
+    }
+    return false;
 };
 
 // Do not edit below this line

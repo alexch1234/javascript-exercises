6ecf5b3fecc51573940abe00f7d96a0d33e649cf
Alex Chang <75132115+alexch1234@users.noreply.github.com>
2023-12-07 19:43:03 +0000

    feat: Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...elementsToRemove) {
+    return arr.filter(element => {
+        for (let elementToRemove of elementsToRemove) {
+            if (element === elementToRemove) {
+                return false;
+            }
+        }
+        return true;
+    });
 };
 
 // Do not edit below this line

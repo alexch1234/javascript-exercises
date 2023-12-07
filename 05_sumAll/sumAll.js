e8c2d08ac76a13e55a902c8905e3a36e30a4631b
Alex Chang <75132115+alexch1234@users.noreply.github.com>
2023-12-07 19:54:40 +0000

    feat: Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0) {
+        return "ERROR";
+    }
+    if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+        return "ERROR";
+    }
+    let sum = 0;
+    let smallerNumber = num1;
+    let largerNumber = num2;
+    if (num1 > num2) {
+        largerNumber = num1;
+        smallerNumber = num2
+    }
+    while (smallerNumber <= largerNumber) {
+        sum += smallerNumber;
+        smallerNumber++;
+    }
+    return sum;
 };
 
 // Do not edit below this line
